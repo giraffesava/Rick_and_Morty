d3fab723fb061b4336a8ffaf0ca91a1ca9062453
Savelii <savely54@inbox.ru>
2021-04-21 20:09:39 +0700

    added provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import Season from 'components/Season/Season'
 import React from 'react'
 
-const App: React.FC = () => <div>hello world</div>
+const App: React.FC = () => (
+  <div>
+    <Season />
+  </div>
+)
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Provider } from 'react-redux'
+import { initStore } from './store/store'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = initStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
