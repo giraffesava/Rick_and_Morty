f33f85fca49268451b5dcb1f4c00149a74453141
Savelii <savely54@inbox.ru>
2021-04-23 23:23:53 +0700

    Added types

---
+++ src/components/types.ts
@@ -0,0 +1,16 @@
+export interface Props {
+  episodes: Episodes
+  season?: number
+  episode?: number
+}
+
+export interface Episodes {
+  episode?: number
+  split?: any
+  map?: any
+  air_date: string
+  characters: Array<string>
+  episodes: string
+  id: number
+  name: string
+}
