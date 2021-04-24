5925fae06ead9fcc6f3529a9edc1ddeb9f029983
Savelii <savely54@inbox.ru>
2021-04-24 15:18:04 +0700

    Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Body from './components/Body/Body'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import EpisodePage from './components/EpisodePage/EpisodePage'
 const App: React.FC = () => (
-  <div>
-    <Body />
-  </div>
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/:id" component={EpisodePage} />
+        <Route path="/" exact component={Body} />
+      </Switch>
+    </div>
+  </Router>
 )
 
 export default App
--- src/components/Episode/Episode.tsx
+++ src/components/Episode/Episode.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import classes from './Episode.module.css'
 import { Props } from './../types'
+import { Link } from 'react-router-dom'
 
 const Episode: React.FC<Props> = ({ episode, episodes }) => {
   console.log('Episode', episodes.characters)
   return (
-    <div className={classes.episodeWrapper}>
-      <div className={classes.everyEpisode}>
-        <p>Episode {episode}</p>
+    <Link to={`/${episodes.id}`} key={episodes.episodes}>
+      <div className={classes.episodeWrapper}>
+        <div className={classes.everyEpisode}>
+          <p>Episode: {episode}</p>
+          <p>Name: {episodes.name}</p>
+          <p>Data: {episodes.air_date}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
