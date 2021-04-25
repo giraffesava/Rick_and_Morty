caef7b10ee530a343f7b6792cc9d477c17ed60f1
Savelii <savely54@inbox.ru>
2021-04-26 02:53:47 +0700

    added buttonReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,10 +3,12 @@ import allEpisodesReducer from './allEpisodes/allEpisodes.reducer'
 import charactersReducer from './characters/characters.reducer'
 import inputEpisodeReducer from './inputEpisode/inputEpisode.reducer'
 import episodeFilterReducer from './episodeFilter/episodeFilter.reducer'
+import buttonReducer from './button/button.reducer'
 
 export const rootReducer = combineReducers({
   allEpisodesReducer,
   charactersReducer,
   inputEpisodeReducer,
   episodeFilterReducer,
+  buttonReducer,
 })
