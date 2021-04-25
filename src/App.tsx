1e61cb91045efc83525becd196735a15c532c50d
Savelii <savely54@inbox.ru>
2021-04-25 23:26:30 +0700

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Body from './components/Body/Body'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import EpisodePage from './components/EpisodePage/EpisodePage'
+import Location from './components/Location/Location'
+
 const App: React.FC = () => (
   <Router>
     <div>
       <Switch>
-        <Route path="/:id" component={EpisodePage} />
+        <Route path="/episode/:id" component={EpisodePage} />
         <Route path="/" exact component={Body} />
+        <Route path="/location/:id" component={Location} />
       </Switch>
     </div>
   </Router>
