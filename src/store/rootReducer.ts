9fc0234fbc6c4076452fbdc840f3f024d2ae514a
Savelii <savely54@inbox.ru>
2021-04-25 23:24:33 +0700

    Added a new reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux'
 import allEpisodesReducer from './allEpisodes/allEpisodes.reducer'
+import charactersReducer from './characters/characters.reducer'
+import inputEpisodeReducer from './inputEpisode/inputEpisode.reducer'
+import episodeFilterReducer from './episodeFilter/episodeFilter.reducer'
 
 export const rootReducer = combineReducers({
   allEpisodesReducer,
+  charactersReducer,
+  inputEpisodeReducer,
+  episodeFilterReducer,
 })
