67fbe1846363edbf767bc6f74a2f7db2ffaf9056
Savelii <savely54@inbox.ru>
2021-04-25 23:24:59 +0700

    Added sagamiddleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,6 +3,9 @@ import { rootReducer } from './rootReducer'
 import createSagaMiddleware from 'redux-saga'
 import { requestWatcher } from './allEpisodes/allEpisodes.saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { getCharactersWatcher } from './characters/characters.saga'
+import { inputEpisodeWatcher } from './inputEpisode/inputEpisode.saga'
+import { episodeFilterWatcher } from './episodeFilter/episodeFilter.saga'
 
 export const initStore = (): any => {
   const sagaMiddleware = createSagaMiddleware()
@@ -11,5 +14,8 @@ export const initStore = (): any => {
     composeWithDevTools(applyMiddleware(sagaMiddleware)),
   )
   sagaMiddleware.run(requestWatcher)
+  sagaMiddleware.run(getCharactersWatcher)
+  sagaMiddleware.run(inputEpisodeWatcher)
+  sagaMiddleware.run(episodeFilterWatcher)
   return store
 }
