8d4fa760091829f77be1a3d9dab3beb899b7f3cf
Savelii <savely54@inbox.ru>
2021-04-26 17:27:04 +0700

    Filled location component

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
-const Location: React.FC<any> = () => {
-  return <div>This page will be later :)</div>
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { locationRequestAction } from './../../store/location/location.actions'
+import { selectLocation } from './../../store/location/location.selector'
+import classes from './Location.module.css'
+import Header from './../Header/Header'
+
+const Location: React.FC<any> = (props) => {
+  const dispatch = useDispatch()
+  const location = useSelector(selectLocation)
+  const { id } = props.match.params
+
+  useState(() => {
+    dispatch(locationRequestAction(id))
+  }, [])
+
+  return (
+    <div>
+      <Header />
+      <img
+        className={classes.wallpaper}
+        src="https://cdn.hipwallpaper.com/i/56/3/o2iEmS.jpg"
+        alt="wallpaper"
+      />
+      <div className={classes.textContainer}>
+        <h1>Name: {location.data.name}</h1>
+        <h1>Type: {location.data.type}</h1>
+        <h1>Dimension: {location.data.dimension}</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Location
