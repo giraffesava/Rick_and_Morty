cf9e58ce2a0c70b0dab46711694953677bbbe8ea
Savelii <savely54@inbox.ru>
2021-05-01 19:03:05 +0700

    added loading check

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Spinner from 'components/Spinner/Spinner'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
@@ -6,12 +7,15 @@ const Wrapper = styled.div`
   background-color: rgba(160, 160, 160, 0.3);
   color: #fff;
   margin-bottom: 20px;
+  display: flex;
+  flex-direction: row;
 `
 
 const Title = styled.h1`
   font-size: 45px;
   margin: 0 10px;
   display: inline-block;
+
   color: rgba(250, 250, 100, 0.8);
   padding: 0 10px;
   &:hover {
@@ -25,6 +29,7 @@ const Header: React.FC<any> = () => {
       <Link to="/">
         <Title>RICK AND MORTY</Title>
       </Link>
+      <Spinner />
     </Wrapper>
   )
 }
